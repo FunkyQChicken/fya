e5063b0e71393a5940eaa3f9e1a0efea39709c0e
Ally <ally@purplish.blue>
2022-11-06 09:15:26 -0500

    finish preliminary tui

--- ui.go
+++ ui.go
@@ -14,7 +14,7 @@ var (
     Width(60).
     Margin(1, 1).
     Padding(1, 1).
-    BorderStyle(lipgloss.RoundedBorder())
+    BorderStyle(lipgloss.ThickBorder())
 
   bodyStyle = lipgloss.NewStyle()
 
@@ -23,16 +23,27 @@ var (
     MarginBottom(1)
 
   titleStyle = lipgloss.NewStyle().
+    Bold(true).
     Foreground(lipgloss.Color("#9AFAFA")).
     Align(lipgloss.Center).
     Padding(0, 2).
     MarginBottom(1).
     BorderStyle(lipgloss.RoundedBorder())
-
       
+	subtle = lipgloss.NewStyle().
+    Foreground(lipgloss.Color("241"))
+
+  right = lipgloss.NewStyle().
+    Align(lipgloss.Right).
+    PaddingLeft(2)
 )
 
 func centsAsDollar(cents int) string {
+  negative := ""
+  if cents < 0 {
+    cents *= -1
+    negative = "-"
+  }
   dollars := cents / 100
   cents    = cents % 100
   var padding string
@@ -41,7 +52,7 @@ func centsAsDollar(cents int) string {
   } else {
     padding = ""
   }
-  return fmt.Sprintf("%d.%s%d", dollars, padding, cents)
+  return fmt.Sprintf("%s$%d.%s%d", negative, dollars, padding, cents)
 }
 
 type app struct {
@@ -109,7 +120,7 @@ func (i chainItem) FilterValue() string { return i.Title() }
 
 type menuItem struct { i item }
 func (i menuItem) Title() string { 
-  return fmt.Sprintf("%s - $%s", i.i.name, centsAsDollar(i.i.cost)) 
+  return fmt.Sprintf("%s - %s", i.i.name, centsAsDollar(i.i.cost)) 
 }
 func (i menuItem) Description() string { 
   return fmt.Sprintf("%s\n%d calories", i.i.description, i.i.calories)
@@ -117,11 +128,18 @@ func (i menuItem) Description() string {
 func (i menuItem) FilterValue() string { return i.i.name }
 
 
+type discountItem struct { d discount }
+func (i discountItem) Title() string       { return i.d.name }
+func (i discountItem) Description() string { return i.d.description }
+func (i discountItem) FilterValue() string { return i.Title() }
+
+
 
 type picker struct {
   list list.Model
   chain chain
   location location
+  foodChosen bool
 }
 
 func initChainPicker() picker {
@@ -131,7 +149,7 @@ func initChainPicker() picker {
   }
   lst := list.New(chainItems, list.NewDefaultDelegate(), 0, 0)
   lst.Title = "Pick your restraunt chain"
-  return picker {lst, nil, nil}
+  return picker {lst, nil, nil, false}
 }
 
 func (p * picker) intoLocPicker() {
@@ -157,7 +175,17 @@ func (p * picker) intoFoodPicker() {
   p.list.SetItems(menuItems)
   p.list.ResetFilter()
   p.list.ResetSelected()
-  list.NewDefaultDelegate()
+}
+
+func (p * picker) intoDiscountPicker(discounts []discount) {
+  discountItems := make([]list.Item, len(discounts))
+  for i, v := range discounts {
+    discountItems[i] = discountItem{v}
+  }
+  p.list.Title = "Pick discounts"
+  p.list.SetItems(discountItems)
+  p.list.ResetFilter()
+  p.list.ResetSelected()
 }
 
 
@@ -191,11 +219,25 @@ func (p picker) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
               p.intoFoodPicker()
               return p, nil
             }
+          } else if !p.foodChosen {
+            men, ok := p.list.SelectedItem().(menuItem)
+            if ok {
+              p.location.AddItem(men.i)
+              discounts := p.location.Discounts()
+              p.foodChosen = true
+              if len(discounts) > 0 {
+                p.intoDiscountPicker(discounts)
+                return p, nil
+              } else {
+                return initCartPreview(p.location), nil
+              }
+            }
           } else {
-            men, _ := p.list.SelectedItem().(menuItem)
-            p.location.AddItem(men.i)
-            // Todo go to discount prompt
-            return p, nil
+            disc, ok := p.list.SelectedItem().(discountItem)
+            if ok {
+              p.location.ApplyDiscounts(disc.d)
+              return initCartPreview(p.location), nil
+            }
           }
       }
     case tea.WindowSizeMsg:
@@ -290,3 +332,44 @@ func (s SignIn) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
   return s, tea.Batch(ucmd, pcmd, foc)
 }
 
+
+
+type cartPreview struct { 
+  location location 
+  items []cartItem
+}
+
+func initCartPreview(location location) cartPreview{
+  return cartPreview {
+    location: location,
+    items: location.Cart(),
+    }   
+}
+
+func (c cartPreview) Init() tea.Cmd { return nil }
+func (c cartPreview) Update(msg tea.Msg) (tea.Model, tea.Cmd) { 
+  switch msg := msg.(type) {
+  case tea.KeyMsg:
+    switch msg.String() {
+      case "y", "Y":
+        c.location.Checkout()
+        header = "Order Placed!"
+        return c, tea.Quit
+      case "n", "N":
+        header = "Order Canceled!"
+        return c, tea.Quit
+    }
+  }
+  return c, nil
+}
+
+func (c cartPreview) View() string {
+  costs := ""
+  items := ""
+  for _, it := range c.items {
+    items += it.description + "\n"
+    costs += centsAsDollar(it.cost) +"\n"
+  }
+  return fmt.Sprintf("%s\nWould you like to place your order?\n%s", 
+    lipgloss.JoinHorizontal(lipgloss.Top, items, right.Render(costs)), subtle.Render("[Y]es/[N]o"))
+}
