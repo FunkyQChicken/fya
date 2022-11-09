331b6867d07bd74d6fc4839da137b357c94832ba
Ally <ally@purplish.blue>
2022-11-09 14:31:25 -0500

    clear log file each run

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
   "log"
 )
 
+const LogFile = "fya.log"
 
 func main() {
 	var f *os.File
-	f, _ = os.OpenFile("fya.log", os.O_CREATE | os.O_RDWR, 0666)
+	f, _ = os.OpenFile(LogFile, os.O_TRUNC | os.O_CREATE | os.O_RDWR, 0666)
   log.SetOutput(f)
   log.Println("#####################################################")
   log.Println("# WARNING, THIS FILE CONTAINS SENSITIVE INFORMATION #")
